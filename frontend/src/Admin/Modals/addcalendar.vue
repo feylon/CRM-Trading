eb8ef26efec2b57a7f500ce3597a23432bac5115
feylon <jamshid14092002@gmail.com>
2024-08-28 13:03:48 +0500

    xatolar sozlandi

--- frontend/src/Admin/Modals/addcalendar.vue
+++ frontend/src/Admin/Modals/addcalendar.vue
@@ -1,34 +1,18 @@
 <template>
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
-    <n-form ref="formRef" inline :label-width="80" :model="formValue" :rules="rules" :size="size"
-        class="flex flex-wrap justify-center">
-
+    <n-form ref="formRef" inline :label-width="80" :model="formValue" :rules="rules" :size="size" class="flex flex-wrap justify-center">
         <n-form-item label="Boshlanish vaqti" path="start_time">
             <n-date-picker v-model:value="formValue.start_time" type="datetime" />
         </n-form-item>
-
-
         <n-form-item label="Tugash vaqti" path="end_time">
             <n-date-picker v-model:value="formValue.end_time" type="datetime" />
         </n-form-item>
-
-
         <n-form-item label="Title" path="title">
             <n-input v-model:value="formValue.title" placeholder="Title" />
         </n-form-item>
-
         <n-form-item label="Joylashuv" path="location">
             <n-input v-model:value="formValue.location" placeholder="Title" />
         </n-form-item>
-
-
         <div class="w-[600px]">
-
             <n-form-item label="Description" path="description">
                 <n-input v-model:value="formValue.description" placeholder="Textarea" type="textarea" :autosize="{
                     minRows: 3,
@@ -36,9 +20,7 @@
                 }" />
             </n-form-item>
         </div>
-
         <div class="w-[600px]">
-
             <n-form-item label="Tags" path="tags">
                 <n-input type="textarea" :autosize="{
                     minRows: 3,
@@ -46,60 +28,34 @@
                 }" v-model:value="formValue.tags" @keydown.space.prevent="spacekey" placeholder="Teglar" />
             </n-form-item>
         </div>
-
         <n-form-item label="Havola" path="url">
             <n-input v-model:value="formValue.url" placeholder="Title" />
         </n-form-item>
-
         <div class="flex justify-end">
-
             <n-form-item>
-<<<<<<< HEAD
-                
-=======
-
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
                 <n-button @click="handleValidateClick">
                     Qo'shish
                 </n-button>
             </n-form-item>
         </div>
     </n-form>
-
-<<<<<<< HEAD
-=======
     <pre>{{ JSON.stringify(formValue, null, 2) }}</pre>
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
 </template>
 
 <script setup>
-import { ref, onMounted, watch } from 'vue';
+import { ref } from 'vue';
 import { Dean } from '../../../Pinia';
 import url from "../../../base";
 import { useRouter } from 'vue-router';
 import { useMessage } from 'naive-ui';
 
-
 let message = useMessage();
 const router = useRouter();
 const store = Dean();
 
-
-<<<<<<< HEAD
-
 const formRef = ref(null);
-
-=======
-// setTimeout(() => {
-//     store.modals.addcalendar.show = false;
-// }, 3000);
-const formRef = ref(null);
-
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
 const size = ref('small');
 const formValue = ref({
-
-
     title: '',
     start_time: null,
     end_time: null,
@@ -110,8 +66,6 @@ const formValue = ref({
 });
 
 const rules = {
-
-
     title: {
         required: true,
         message: 'Titleni kiriting',
@@ -123,47 +77,28 @@ const rules = {
         trigger: ["blur", "change"],
         message: "Boshlanish vaqtni belgilang"
     },
-
     end_time: {
         type: "number",
         required: true,
         trigger: ["blur", "change"],
         message: "Tugash vaqtini belgilang"
     },
-
-
     description: {
         required: true,
         trigger: ["blur", "input"],
         message: "Descriptionni kiriting"
     },
     tags: {
-<<<<<<< HEAD
-
-=======
-        // type: "number",
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
         required: false,
         trigger: ["blur", "change"],
         message: "Taglarni kiriting"
     },
     location: {
-<<<<<<< HEAD
-
-=======
-        // type: "number",
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
         required: false,
         trigger: ["blur", "change"],
-        message: "Joylasuhvni kiriting"
+        message: "Joylashuvni kiriting"
     },
-
     url: {
-<<<<<<< HEAD
-
-=======
-        // type: "number",
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
         required: false,
         trigger: ["blur", "change"],
         message: "Havolani kiriting"
@@ -181,13 +116,8 @@ const handleValidateClick = async (e) => {
                     if (formValue.value[i]) {
                         data[i] = formValue.value[i];
                     }
-
                 }
-<<<<<<< HEAD
                 if (data.url) data.url = `https://${data.url}`;
-=======
-                console.log(data)
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
 
                 let backend = await fetch(`${url}calendar/addcalendar`, {
                     method: "POST",
@@ -198,10 +128,6 @@ const handleValidateClick = async (e) => {
                     body: JSON.stringify(data)
                 });
 
-<<<<<<< HEAD
-=======
-                console.log(backend.status);
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
                 if (backend.status == 400) {
                     backend = await backend.json();
                     message.error(backend.error);
@@ -213,67 +139,28 @@ const handleValidateClick = async (e) => {
                     message.success("Saqlandi");
                     store.modals.addcalendar.show = false;
                     router.go(0);
-
-
                     return;
                 }
 
             } catch (error) {
-<<<<<<< HEAD
-
+                console.log(error);
             }
         } else {
-
-            message.error('Formani to`ldiring');
-=======
-                console.log(error)
-            }
-        } else {
-            console.log(errors);
             message.error('Invalid');
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
         }
     });
 };
 
-
 let spacekey = function () {
-<<<<<<< HEAD
-
-=======
-    console.log('space');
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
     if (formValue.value.tags) {
         formValue.value.tags = formValue.value.tags.trim();
-
         formValue.value.tags = formValue.value.tags
-<<<<<<< HEAD
             .split(' ')
             .map(word => `#${word}`)
             .join(' ') + ' ';
         formValue.value.tags = formValue.value.tags.replace(/#{2,}/g, '#');
-
-
     }
-
-=======
-            .split(' ')        // Split the string by spaces into an array
-            .map(word => `#${word}`)  // Add '#' to each word
-            .join(' ') + ' ';
-        formValue.value.tags = formValue.value.tags.replace(/#{2,}/g, '#');
-
-        // Join the array back into a string with spaces
-    }
-    console.log(formValue.value.tags);
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
-
-
-
-}
-
+};
 </script>
-<<<<<<< HEAD
-=======
 
 <style lang="scss" scoped></style>
->>>>>>> 0e2964ed275a97c2f0cf0b22b2ce68a888363237
